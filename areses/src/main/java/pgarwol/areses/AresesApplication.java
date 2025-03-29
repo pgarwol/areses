fe84a91b1d94e2ba862a7cf2237a42c6f6f62d91
pgarwol <keverth@tutanota.com>
2025-03-29 18:43:21 +0100

    Java Setup

---
+++ areses/src/main/java/pgarwol/areses/AresesApplication.java
@@ -0,0 +1,13 @@
+package pgarwol.areses;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AresesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AresesApplication.class, args);
+	}
+
+}
---
+++ areses/src/test/java/pgarwol/areses/AresesApplicationTests.java
@@ -0,0 +1,13 @@
+package pgarwol.areses;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AresesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
